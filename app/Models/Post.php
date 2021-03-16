9646459c90cfcf94282d7049b77bfc35eca1f034
scmminaungpaing <scm.minaungpaing@gmail.com>
2021-03-16 10:55:42 +0630

    fix

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -12,9 +12,4 @@ class Post extends Model
     public function user(){
         return $this->belongsTo(User::class);
     }
-
-    public static function getPostData(){
-        $record = DB::table('posts')->select('id','title','description','user_id','publish')->orderBy('created_at','asc')->get()->toArray();
-        return $record;
-    }
 }
